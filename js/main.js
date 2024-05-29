657be5c245b9f5a9319b08cbea1b8deaeb9674b0
MatheusReis97 <Matheus.drs97@hotmail.com>
2024-05-29 15:49:19 -0300

    Rev. apresentação inicial
    Refeito a apresentação inicial, e organização para site "responsivel".

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ class FormSubmit{
             await fetch(this.url,{
                 method:"POST",
                 headers: {
-                    "Content-Type":"application/json",
+                    "Content-Type":"application/json",  
                     Accept: "application/json",
                 },
                 body: JSON.stringify(this.getFormObject()),
