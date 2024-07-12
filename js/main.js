a2f172a8847d08fa53b8951eea5a884b3b6d1866
MatheusReis97 <Matheus.drs97@hotmail.com>
2024-07-12 18:43:31 -0300

    realizado reparo css e js
    organizado e melhorado na resposividade, config para email adaptado.

--- js/main.js
+++ js/main.js
@@ -1,65 +1,76 @@
-class FormSubmit{
-    constructor (configuracoes){
+class FormSubmit {
+    constructor(configuracoes) {
         this.configuracoes = configuracoes;
         this.form = document.querySelector(configuracoes.form);
         this.formButton = document.querySelector(configuracoes.button);
-        if (this.form){
-            this.url = this.form.getAttibute("action");
+        if (this.form) {
+            this.url = this.form.getAttribute("action");
         }
-            this.sendForm = this.sendForm.bind(this);
+        this.sendForm = this.sendForm.bind(this);
     }
 
-    displaySucess(){
-        this.form.innerHTML = this.configuracoes.sucess;
+    displaySuccess() {
+        this.form.innerHTML = this.configuracoes.success;
     }
 
-    displayError(){
+    displayError() {
         this.form.innerHTML = this.configuracoes.error;
     }
 
-    getFormObject(){
+    getFormObject() {
         const formObject = {};
         const fields = this.form.querySelectorAll("[name]");
         fields.forEach((field) => {
-            formObject[field.getAttibute("name")] = field.value;
+            formObject[field.getAttribute("name")] = field.value;
         });
         return formObject;
     }
 
-    onSubmission(event){
-        event.preventDefault();
-        event.target.disabled = true;
-        event.target.innerText = " Aguarde ..."
+    onSubmission(event) {
+        console.log("Submitting form");
+        event.preventDefault(); // Prevenir comportamento padrão de envio do formulário
+        this.formButton.disabled = true;
+        this.formButton.innerText = "Aguarde...";
     }
 
-    async sendForm(event){
+    async sendForm(event) {
+        this.onSubmission(event);
         try {
-            this.onSubmission(event);
-            await fetch(this.url,{
-                method:"POST",
+            console.log("Sending form data");
+            const response = await fetch(this.url, {
+                method: "POST",
                 headers: {
-                    "Content-Type":"application/json",  
+                    "Content-Type": "application/json",
                     Accept: "application/json",
                 },
                 body: JSON.stringify(this.getFormObject()),
             });
-            this.displaySucess();
-        } catch (error){
+            if (response.ok) {
+                console.log("Form submitted successfully");
+                this.displaySuccess();
+            } else {
+                console.log("Form submission failed");
+                this.displayError();
+            }
+        } catch (error) {
+            console.log("Error during form submission:", error);
             this.displayError();
-            throw new Error (error);
         }
     }
 
-    init(){
-        if (this.form) this.formButton.addEventListener("click", this.sendForm);
-        return this;
+    init() {
+        if (this.form) {
+            this.form.addEventListener("submit", this.sendForm);
+        }
+        return false; // Retorna false para prevenir comportamento padrão do navegador
     }
 }
 
-const formSubmit = new FormSubmit ({
+const formSubmit = new FormSubmit({
     form: "[data-form]",
     button: "[data-button]",
-    sucess: "<h1 class='sucess'>Mensagem enviada com sucesso</h1>",
-    error: "<h1 class='error'>Não foi possivel enviar sua mensagem</h1>"
+    success: "<h1 class='success'>Mensagem enviada com sucesso</h1>",
+    error: "<h1 class='error'>Não foi possível enviar sua mensagem, TENTE NOVAMENTE</h1>"
 });
+
 formSubmit.init();
